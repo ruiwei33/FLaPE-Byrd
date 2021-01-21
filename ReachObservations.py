236996e329f6249d7c295bb2da8be832be1188a9
mikedurand <durand.8@osu.edu>
2021-01-21 14:28:19 -0500

    update ReachObservations to best practices

--- ReachObservations.py
+++ ReachObservations.py
@@ -2,7 +2,6 @@
 # -*- coding: utf-8 -*-
 """
 Created on Wed Jan 13 23:49:40 2021
-
 @author: mtd
 """
 
@@ -24,39 +23,26 @@ class ReachObservations:
         self.sigS=RiverData["sigS"]
         
         # et al.
-        self.GetVectorObs()            
+        #self.GetVectorObs()            
         
-        self.CalcdA()
-    
-    def GetVectorObs(self):    
-        
+        #self.CalcdA()
+
         #%% create resahepd versions of observations
         self.hv=reshape(self.h, (self.D.nR*self.D.nt,1) )
         self.Sv=reshape(self.S, (self.D.nR*self.D.nt,1) )
         self.wv=reshape(self.w, (self.D.nR*self.D.nt,1) )
 
-  
-
-            
-    def CalcdA(self):
-    
         DeltaAHat=empty( (self.D.nR,self.D.nt-1) )
+        self.DeltaAHatv = self.calcDeltaAHatv(DeltaAHat)
+        self.dA= concatenate(  (zeros( (self.D.nR,1) ), DeltaAHat @ triu(ones( (self.D.nt-1,self.D.nt-1) ),0)),1 )
+        self.dAv=self.D.CalcU() @ self.DeltaAHatv
+
+    def calcDeltaAHatv(self, DeltaAHat):
         
         for r in range(0,self.D.nR):
             for t in range(0,self.D.nt-1):
                 DeltaAHat[r,t]=(self.w[r,t]+self.w[r,t+1])/2 * (self.h[r,t+1]-self.h[r,t])
          
         # changed how this part works compared with Matlab, avoiding translating calcU
-        DeltaAHatv=reshape(DeltaAHat,(self.D.nR*(self.D.nt-1),1) )
-        
-        self.dA= concatenate(  (zeros( (self.D.nR,1) ), DeltaAHat @ triu(ones( (self.D.nt-1,self.D.nt-1) ),0)),1 ) 
-                
-        U=self.D.CalcU()
-        self.dAv=U @ DeltaAHatv
-    
-        # Note that the below commented command is equivalent, but "U" is needed in
-        #   other functions, and this is a good test for consistency
-        #Obs.dAv=reshape(Obs.dA, (D.nR*D.nt,1)) 
-        
-
+        return reshape(DeltaAHat,(self.D.nR*(self.D.nt-1),1) )
         
